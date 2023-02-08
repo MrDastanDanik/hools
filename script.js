bf2a74b7af7dceecd299fabb196c6cac1101df8f
MrDastanDanik <12bignik135@gmail.com>
2023-02-08 17:54:11 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://hools.online/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hools.online
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+setInterval(function(){
+    location.reload();
+}, 30000);
+
+let work = $(".col-auto.align-self-center.mx-auto")[8].innerText;
+let stor = location.pathname;
+
+
+switch (stor) {
+    case '/game':
+        console.log(stor);
+        if (work >=20 ) {;
+            $(location).attr('href', "work")
+        }
+        break;
+    case '/district':
+        break;
+  case '/work':
+        if (work >=20 ) {
+            //if ($(".row a")[9].text== "Я берусь!" || "Выполнить"){
+              //  $(location).attr('href', $(".row a")[9].href);
+            //}
+
+            var findText = "Выполнить";
+            var $elements = $(".container *").filter(function(){
+                return $(this).html() == findText;
+            });
+            $elements.each(function() { this.click() });
+
+            findText = "Я берусь!"
+            $elements.each(function() { this.click() });
+        }
+        break;
+}
+
+
+
+
+    // Your code here...
+})();

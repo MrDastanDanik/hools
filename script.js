4af2256342e2a2d7b1416d533bf64b8f2901f1a4
MrDastanDanik <12bignik135@gmail.com>
2023-02-08 21:54:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         New Userscript
 // @namespace    http://tampermonkey.net/
-// @version      0.1.4
+// @version      0.1.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://hools.online/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hools.online
+// @downloadURL  https://raw.githubusercontent.com/MrDastanDanik/hools/main/script.js
 // @updateURL    https://raw.githubusercontent.com/MrDastanDanik/hools/main/script.js
 // @grant        none
 // ==/UserScript==

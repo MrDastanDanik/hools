c25ec21720eb9a5ec1f49ac763c5d683c585c37e
MrDastanDanik <12bignik135@gmail.com>
2023-03-14 18:01:53 +0200

    Update Hools.user.js

--- Hools.user.js
+++ Hools.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Hools
-// @version      0.1.29
+// @version      0.1.3
 // @description  try to take over the world!
 // @author       DaniilShe
 // @match        https://hools.online/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=hools.online
+// @icon         https://icon-icons.com/downloadimage.php?id=4068&root=38/PNG/32/&file=closeupmode_close_4630.png
 // @downloadURL  https://github.com/MrDastanDanik/hools/raw/main/Hools.user.js
 // @updateURL    https://github.com/MrDastanDanik/hools/raw/main/Hools.user.js
 // @require      https://github.com/MrDastanDanik/hools/raw/main/script.user.js
@@ -13,8 +13,5 @@
 // ==/UserScript==
 (function() {
     'use strict';
-    if ($('h1').innerText == "502 Bad Gateway") {
-        location.reload();
-    }
-    setInterval(function(){location.reload()}, 1200000)
+
 })();

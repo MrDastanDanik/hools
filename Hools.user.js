17c6deba88c1436f8324d5647d1f3443b104cb98
MrDastanDanik <12bignik135@gmail.com>
2023-03-14 10:33:30 +0200

    Update Hools.user.js

--- Hools.user.js
+++ Hools.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Hools
-// @version      0.1.22
+// @version      0.1.23
 // @description  try to take over the world!
 // @author       DaniilShe
 // @match        https://hools.online/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hools.online
 // @require      https://github.com/MrDastanDanik/hools/raw/main/script.user.js
+// @downloadURL  https://github.com/MrDastanDanik/hools/raw/main/Hools.user.js
+// @updateURL    https://github.com/MrDastanDanik/hools/raw/main/Hools.user.js
 // @grant        none
 // ==/UserScript==
 (function() {

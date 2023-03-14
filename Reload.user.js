55740b5c1981e8798453224555bbf003512771f1
MrDastanDanik <12bignik135@gmail.com>
2023-03-14 18:01:19 +0200

    Update and rename reload.js to Reload.user.js

---
+++ Reload.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Reload
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       DaniilShe
+// @match        http://*/*
+// @icon         https://icon-icons.com/downloadimage.php?id=4068&root=38/PNG/32/&file=closeupmode_close_4630.png
+// @require      https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    try {}
+    catch {
+        /*if ($('h1').innerText == "502 Bad Gateway") {
+            location.reload();
+        }*/
+        location.reload();
+    }
+
+})();

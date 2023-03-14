3c9003dd099c47359795de619487a86136eb331b
MrDastanDanik <12bignik135@gmail.com>
2023-03-14 09:13:43 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,5 +1,5 @@
 // @version      0.1.22
-
+alert('test');
 setTimeout(function() {
     let work = $(".col-auto.align-self-center.mx-auto")[8].innerText;
     let health = $(".col-auto.align-self-center.mx-auto")[7].innerText;

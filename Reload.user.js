fa346d7d6e4dbe6ac929149db7699f2f18f001c8
MrDastanDanik <12bignik135@gmail.com>
2023-03-16 11:00:49 +0200

    Update Reload.user.js

--- Reload.user.js
+++ Reload.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         Reload
 // @namespace    http://tampermonkey.net/
-// @version      0.1.22
+// @version      0.1.23
 // @description  try to take over the world!
 // @author       DaniilShe
 // @match        http://*/*
 // @match        https://hools.online/*
 // @icon         https://icon-icons.com/downloadimage.php?id=4068&root=38/PNG/32/&file=closeupmode_close_4630.png
-// @require      https://code.jquery.com/jquery-3.2.1.slim.min.js
 // @grant        none
 // ==/UserScript==
 
